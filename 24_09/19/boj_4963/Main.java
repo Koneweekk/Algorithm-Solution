1d6b83d3e84ee1bb538e2eee7ec02f7c1df295fb
김한주 <rlagkswn37@naver.com>
2024-09-19 19:17:30 +0900

    [240919] solving 0919 problem

---
+++ 24_09/19/boj_4963/Main.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  // 입출력 관련 필드
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  StringBuffer sb = new StringBuffer();
+  StringTokenizer st;
+  // 공통 변수 필드
+  int[][] directions = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };  // 이동 방향
+  int w;  // 지도 가로 크기
+  int h;  // 지도 세로 크기
+  int[][] mapArray;  // 지도 배열
+  int count;  // 섬의 개수
+
+
+  // 지도 순회 함수
+  private void findIsland(int[] start) {
+    // 1. dfs를 위한 스택 선언
+    Stack<int[]> stack = new Stack<>();
+    stack.add(start);
+    mapArray[start[0]][start[1]] = 0;
+
+    // 2. dfs를 통해 현재 위치와 인접한 육지 탐색
+    while (!stack.isEmpty()) {
+      int[] now = stack.pop();
+      for (int[] dir : directions) {
+        // 2.1 다음 진행 방향
+        int nr = now[0] + dir[0];
+        int nc = now[1] + dir[1];
+        // 2.2 지도 밖을 벗어난 경우 다음 방향 탐색
+        if (nr < 0 || nr >= h || nc < 0 || nc >= w) continue;
+        // 2.3 바다인 경우 다음 방향 탐색
+        if (mapArray[nr][nc] == 0) continue;
+        // 2.4 육지인 경우 스택에 넣고 지도에 표시
+        stack.add(new int[]{nr, nc});
+        mapArray[nr][nc] = 0;
+      }
+    }
+  } 
+
+
+  // 테스트 케이스 실행 함수
+  private void solveTestCase() throws IOException {
+    // 1. 지도값 할당
+    mapArray = new int[h][w];
+    for (int r = 0; r < h; r++) {
+      st = new StringTokenizer(br.readLine());
+      for (int c = 0; c < w; c++) {
+        mapArray[r][c] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    // 2. 지도를 순회하며 섬 개수 체크
+    count = 0;
+    for (int r = 0; r < h; r++) {
+      for (int c = 0; c < w; c++) {
+        // 2.1 바다인 경우 다음 위치 탐색
+        if (mapArray[r][c] == 0) continue;
+        // 2.2 섬인 경우 섬의 크기 탐색
+        count++;
+        findIsland(new int[]{r, c});
+      }
+    }
+
+    // 3. 정답값 입력
+    sb.append(count).append("\n");
+  }
+
+
+  // 주요 실행 함수
+  private void run() throws IOException {
+    while (true) {
+      // 배열 크기 할당
+      String[] size = br.readLine().split(" ");
+      // 종료 조건
+      if (size[0].equals("0")) {
+        break;
+      }
+      // 그 외의 경우엔 테스트케이스 실행
+      w = Integer.parseInt(size[0]);
+      h = Integer.parseInt(size[1]);
+      solveTestCase();
+    }
+
+    // 정답 출력
+    System.out.println(sb.toString().trim());
+  }
+
+
+  // 메인 함수
+  public static void main(String[] args) throws IOException {
+    Main main = new Main();
+    main.run();
+  }
+}
